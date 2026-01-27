87d710686fe5d81e2f62aa786da1fa24ef61ca7b
Marvin Schoeller <git@marvinschoeller.de>
2026-01-27 15:04:16 +0100

    Add initial implementations for spindle speed check and prime number verification

---
+++ lessons/V06-Programm-Ablauf-Plaene-Teil2/listen-beispiel.py
@@ -0,0 +1,16 @@
+# messwerte = [27,2,5,8,11,13]
+# messwerte2 = ["W", "e", "l", "t"]
+# i = -2
+# print(messwerte[i])
+
+# messwerte3 = [messwerte, messwerte2]
+# print(messwerte3)
+
+# messwerte3.append(["r","t"])
+# print(messwerte3)
+summe = 0
+for i in range(10000):
+    print(i)
+    summe += i # summe = summe +i
+
+print(f"summe: {summe}")
